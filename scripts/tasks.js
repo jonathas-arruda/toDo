c5f41d824ffd47b4a02499ba6197f886e4df557b
Jonathas <jonathasarruda@outlook.com>
2022-07-05 18:36:00 -0300

    Creating functionalities to create and rendering tasks

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,83 +1,94 @@
-const nomeUsuario = document.getElementById('nomeUsuario');
-//utilizamos o metodo onload para efetuar a funcao de inserir o nome quando a pagina for carregada.
-window.onload = ()=> {
-  receberUsuario()
+const nomeUsuario = document.getElementById("nomeUsuario");
+const listaPendente = document.getElementById("tarefasPendentes");
+const listaTerminadas = document.getElementById("tarefasTerminadas");
+
+window.onload = () => {
+  receberUsuario();
+  listarTarefas();
 };
 
+const jwt = localStorage.getItem("token");
+
 function receberUsuario() {
-
-  const jwt = localStorage.getItem("token");
-
   const configuracaoRequisicao = {
-
     method: "GET",
     headers: {
       "Content-type": "application/json",
-      "Authorization": jwt 
-    }
-  }
+      Authorization: jwt,
+    },
+  };
 
-  fetch('https://ctd-todo-api.herokuapp.com/v1/users/getMe', configuracaoRequisicao)
+  fetch(
+    "https://ctd-todo-api.herokuapp.com/v1/users/getMe",
+    configuracaoRequisicao
+  )
+    .then((response) => response.json())
 
-    .then(response => response.json())
-
-    .then(data => {
-      const dados = `${data.firstName} ${data.lastName}`
+    .then((data) => {
+      const dados = `${data.firstName} ${data.lastName}`;
       nomeUsuario.innerHTML = dados;
     })
 
     .catch((err) => {
-      console.log(err)
-
-    })
+      console.log(err);
+    });
 }
 
+function renderizarTarefas(tasks) {
+  listaPendente.innerHTML = "";
+  listaTerminadas.innerHTML = "";
 
-//PASSO 2
+  setTimeout(() => {
+    tasks.foreach( task => {
+      const dataFormatada = new Date(task.createdAt).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "2-digit",
+          year: "numeric",
+        }
+      );
 
+      if (task.completed) {
+        listaTerminadas.innerHTML = `<li class="tarefa">
+                <div class="not-done"
+                onclick="RemoverTarefa(${task.id})></div>
+                <div class="descricao">
+                  <p class="nome">${task.description}</p>
+                  <p class="timestamp"> "Criada em:" ${dataFormatada}</p>u
+                </div>
+              </li>`;
+      } else {
+        listaPendente.innerHTML = `
+        <li class="tarefa">
+        <div class="not-done" onclick="atualizarTarefa(${task.id},true)"></div>
+        <div class="descricao">
+          <p class="nome">${task.description}</p>
+          <p class="timestamp"> Criada em: ${dataFormatada}</p>
+        </div>
+      </li>`;
+      }
+    });
+  }, 1000);
+}
 
-// UTILIZAR ENDPOINT /TASKS E LISTAR NO CONSOLE.
-// TAMBEM ACRESCENTAR A FUNCAO QUE LISTA AS TAREFAS EM NOSSO WINDOW.ONLOAD
-//no segundo then do fetch:
-//Utilizem template string, para através do JS inserir novamente no HTML o corpo das tarefas
+function listarTarefas() {
+  const configuracaoRequisicao = {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+      Authorization: jwt,
+    },
+  };
 
-// `<li class="tarefa">
-//         <div class="not-done"></div>
-//         <div class="descricao">
-//           <p class="nome">${data.description}</p>
-//           <p class="timestamp"> "Criada em:" ${data.createdAt}</p>
-//         </div>
-//       </li>`
+  fetch("https://ctd-todo-api.herokuapp.com/v1/tasks", configuracaoRequisicao)
+    .then((response) => response.json())
 
+    .then((data) => {
+      renderizarTarefas(data);
+    })
 
-//PASSO 3:
-
-//utilizar endpoint  /tasks para criar uma tarefa
-
-
-// utilizar o botão + para efetuar o fetch desse endpoint
-//Utilizando o evento de clicar.
-//inserir usando innerHTML e template string, a nova tarefa criada ao 
-//clicar no botão.
-
-//criar uma variavel onde utilize o metodo new Date
-
-//const dataFormata = newDate(data.createdAt).toLocaleDateString(
-
-  // 'pt-BR',
-  // {
-  //     day: '2-digit',
-  //     month: '2-digit',
-  //     year: 'numeric'
-  // }
-
-
-//)
-
-// `<li class="tarefa">
-//         <div class="not-done"></div>
-//         <div class="descricao">
-//           <p class="nome">${data.description}</p>
-//           <p class="timestamp"> "Criada em:" ${dataFormatada}</p>
-//         </div>
-//       </li>`
+    .catch((err) => {
+      console.log(err);
+    });
+}
